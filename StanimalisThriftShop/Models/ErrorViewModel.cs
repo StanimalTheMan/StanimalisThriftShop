d28d87550f08459e325aaa49df6be52b036a3c3a
Stan Choi <sjc591@nyu.edu>
2024-02-08 17:10:31 -0500

    Add project files.

---
+++ StanimalisThriftShop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StanimalisThriftShop.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
