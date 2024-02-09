030884d9231956f8e1840d1bac889aaf754a3874
Stan Choi <sjc591@nyu.edu>
2024-02-09 09:12:45 -0500

    Add Category Controller

---
+++ StanimalisThriftShop/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StanimalisThriftShop.Controllers
+{
+	public class CategoryController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
