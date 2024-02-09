9dc9b0d4f03eda7df4e93b8862954a1b27aebf68
Stan Choi <sjc591@nyu.edu>
2024-02-09 06:33:05 -0500

    Add Category Model

---
+++ StanimalisThriftShop/Models/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace StanimalisThriftShop.Models;
+
+public class Category
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int DisplayOrder { get; set; }
+}
