4ccd847ed289f15c3b993e9c49d158f6f1795f57
Stan Choi <sjc591@nyu.edu>
2024-02-09 07:16:35 -0500

    Add data annotations

--- StanimalisThriftShop/Models/Category.cs
+++ StanimalisThriftShop/Models/Category.cs
@@ -1,8 +1,12 @@
-﻿namespace StanimalisThriftShop.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StanimalisThriftShop.Models;
 
 public class Category
 {
+    [Key]
     public int Id { get; set; }
+    [Required]
     public string Name { get; set; }
     public int DisplayOrder { get; set; }
 }
