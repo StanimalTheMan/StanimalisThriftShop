1e63b6b91ee476a56eaae3708f3ba01fefca57d2
Stan Choi <sjc591@nyu.edu>
2024-02-09 09:34:30 -0500

    Seed Category Table

--- StanimalisThriftShop/Data/ApplicationDbContext.cs
+++ StanimalisThriftShop/Data/ApplicationDbContext.cs
@@ -11,4 +11,13 @@ public class ApplicationDbContext : DbContext
     }
 
     public DbSet<Category> Categories { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>().HasData(
+            new Category { Id = 1, Name = "Jerseys", DisplayOrder = 1 },
+            new Category { Id = 2, Name = "Caps", DisplayOrder = 2 },
+            new Category { Id = 3, Name = "Collectibles", DisplayOrder = 3 }
+            );
+    }
 }
