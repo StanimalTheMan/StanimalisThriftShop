273cacbed29f57d3406657efbe7b24698dd88617
Stan Choi <sjc591@nyu.edu>
2024-02-11 07:01:53 -0500

    Add a custom validation

--- StanimalisThriftShop/Controllers/CategoryController.cs
+++ StanimalisThriftShop/Controllers/CategoryController.cs
@@ -24,6 +24,9 @@ public class CategoryController : Controller
     [HttpPost]
     public IActionResult Create(Category obj)
     {
+        if (obj.Name == obj.DisplayOrder.ToString()) {
+            ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+        }
         if (ModelState.IsValid)
         {
             _db.Categories.Add(obj);
