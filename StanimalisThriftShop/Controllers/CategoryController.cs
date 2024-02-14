f86f0fe9dc42d74d1d12c6fbc1264aaa8882eebc
Stan Choi <sjc591@nyu.edu>
2024-02-14 15:19:15 -0500

    Use TempData to display notifications upon creating, updating, deleting categories

--- StanimalisThriftShop/Controllers/CategoryController.cs
+++ StanimalisThriftShop/Controllers/CategoryController.cs
@@ -33,6 +33,7 @@ public class CategoryController : Controller
         {
             _db.Categories.Add(obj);
             _db.SaveChanges();
+			TempData["success"] = "Category created successfully";
 			return RedirectToAction("Index");
 		}
         return View();
@@ -62,6 +63,7 @@ public class CategoryController : Controller
 		{
 			_db.Categories.Update(obj);
 			_db.SaveChanges();
+			TempData["success"] = "Category updated successfully";
 			return RedirectToAction("Index");
 		}
 		return View();
@@ -92,6 +94,7 @@ public class CategoryController : Controller
 		}
 		_db.Categories.Remove(obj);
 		_db.SaveChanges();
+		TempData["success"] = "Category deleted successfully";
 		return RedirectToAction("Index");
 	}
 }
