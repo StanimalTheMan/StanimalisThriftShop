1166f2975eb9ac3609cdeaf19ce2f080e7e39c6e
Stan Choi <sjc591@nyu.edu>
2024-02-14 15:26:24 -0500

    Impl reusable notification partial

---
+++ StanimalisThriftShop/Views/Shared/_Notification.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StanimalisThriftShop.Views.Shared
+{
+    public class _NotificationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
