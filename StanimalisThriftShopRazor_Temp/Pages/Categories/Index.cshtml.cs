4baf7285f67f7886c0a9c0418b5466a287f2f7dc
Stan Choi <sjc591@nyu.edu>
2024-02-16 11:24:56 -0500

    Display all Categories

--- StanimalisThriftShopRazor_Temp/Pages/Categories/Index.cshtml.cs
+++ StanimalisThriftShopRazor_Temp/Pages/Categories/Index.cshtml.cs
@@ -1,10 +1,20 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using StanimalisThriftShopRazor_Temp.Data;
+using StanimalisThriftShopRazor_Temp.Models;
 
 namespace StanimalisThriftShopRazor_Temp.Pages.Categories;
 
 public class IndexModel : PageModel
 {
-	public void OnGet()
+	private readonly ApplicationDbContext _db;
+    public List<Category> CategoryList { get; set; }
+    public IndexModel(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+
+    public void OnGet()
 	{
+        CategoryList = _db.Categories.ToList();
 	}
 }
