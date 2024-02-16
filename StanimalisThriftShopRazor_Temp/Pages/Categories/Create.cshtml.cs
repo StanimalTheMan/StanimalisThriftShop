cfdce7e52fa43cafd728c31c7a9202e481c78a62
Stan Choi <sjc591@nyu.edu>
2024-02-16 15:06:17 -0500

    Create Create Category UI

---
+++ StanimalisThriftShopRazor_Temp/Pages/Categories/Create.cshtml.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using StanimalisThriftShopRazor_Temp.Data;
+using StanimalisThriftShopRazor_Temp.Models;
+
+namespace StanimalisThriftShopRazor_Temp.Pages.Categories;
+
+public class CreateModel : PageModel
+{
+    private readonly ApplicationDbContext _db;
+    public Category Category { get; set; }
+    public CreateModel(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+    public void OnGet()
+    {
+    }
+}
