b2f659f8bab0c4ff7179c0f000f657637e88aa31
Stan Choi <sjc591@nyu.edu>
2024-02-17 15:03:23 -0500

    Implement ICategoryRepository

---
+++ StanimalisThriftShop.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using StanimalisThriftShop.Models;
+
+namespace StanimalisThriftShop.DataAccess.Repository.IRepository;
+
+public interface ICategoryRepository : IRepository<Category>
+{
+	void Update(Category obj);
+	void Save();
+}
