8555d8610331aa4b26d78010d117894a027e286b
Stan Choi <sjc591@nyu.edu>
2024-02-17 14:34:43 -0500

    Implement Repository Interface

--- StanimalisThriftShop.DataAccess/Repository/IRepository/IRepository.cs
+++ StanimalisThriftShop.DataAccess/Repository/IRepository/IRepository.cs
@@ -2,7 +2,7 @@
 
 namespace StanimalisThriftShop.DataAccess.Repository.IRepository
 {
-	internal interface IRepository<T> where T : class
+	public interface IRepository<T> where T : class
 	{
 		//T - Category
 		IEnumerable<T> GetAll();
