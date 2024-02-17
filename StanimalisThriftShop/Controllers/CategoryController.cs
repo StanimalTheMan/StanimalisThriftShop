22d586fc94bf9ec1646bf468f500bfb3feb4cfeb
Stan Choi <sjc591@nyu.edu>
2024-02-17 15:50:13 -0500

    Replace DbContext with Category Repository

--- StanimalisThriftShop/Controllers/CategoryController.cs
+++ StanimalisThriftShop/Controllers/CategoryController.cs
@@ -1,59 +1,59 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using StanimalisThriftShop.DataAccess.Data;
+using StanimalisThriftShop.DataAccess.Repository.IRepository;
 using StanimalisThriftShop.Models;
 
 namespace StanimalisThriftShop.Controllers;
 
 public class CategoryController : Controller
 {
-    private readonly ApplicationDbContext _db;
-    public CategoryController(ApplicationDbContext db)
-    {
-        _db = db;
-    }
-    public IActionResult Index()
-    {
-        List<Category> objCategoryList = _db.Categories.ToList();
-        return View(objCategoryList);
-    }
+	private readonly ICategoryRepository _categoryRepo;
+	public CategoryController(ICategoryRepository db)
+	{
+		_categoryRepo = db;
+	}
+	public IActionResult Index()
+	{
+		List<Category> objCategoryList = _categoryRepo.GetAll().ToList();
+		return View(objCategoryList);
+	}
 
-    public IActionResult Create()
-    {
-        return View();
-    }
-    [HttpPost]
-    public IActionResult Create(Category obj)
-    {
-        if (obj.Name == obj.DisplayOrder.ToString())
-        {
-            ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
-        }
+	public IActionResult Create()
+	{
+		return View();
+	}
+	[HttpPost]
+	public IActionResult Create(Category obj)
+	{
+		if (obj.Name == obj.DisplayOrder.ToString())
+		{
+			ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+		}
 
-        if (ModelState.IsValid)
-        {
-            _db.Categories.Add(obj);
-            _db.SaveChanges();
+		if (ModelState.IsValid)
+		{
+			_categoryRepo.Add(obj);
+			_categoryRepo.Save();
 			TempData["success"] = "Category created successfully";
 			return RedirectToAction("Index");
 		}
-        return View();
+		return View();
 
-    }
+	}
 
 	public IActionResult Edit(int? id)
 	{
-        if(id==null || id == 0)
-        {
-            return NotFound();
-        }
-        Category? categoryFromDb = _db.Categories.Find(id);
-        //Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u=>u.Id==id);
-        //Category? categoryFromDb2 = _db.Categories.Where(u=>u.Id==id).FirstOrDefault();
+		if (id == null || id == 0)
+		{
+			return NotFound();
+		}
+		Category? categoryFromDb = _categoryRepo.Get(u => u.Id == id);
+		//Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u=>u.Id==id);
+		//Category? categoryFromDb2 = _db.Categories.Where(u=>u.Id==id).FirstOrDefault();
 
-        if (categoryFromDb == null)
-        {
-            return NotFound();
-        }
+		if (categoryFromDb == null)
+		{
+			return NotFound();
+		}
 		return View(categoryFromDb);
 	}
 	[HttpPost]
@@ -61,8 +61,8 @@ public class CategoryController : Controller
 	{
 		if (ModelState.IsValid)
 		{
-			_db.Categories.Update(obj);
-			_db.SaveChanges();
+			_categoryRepo.Update(obj);
+			_categoryRepo.Save();
 			TempData["success"] = "Category updated successfully";
 			return RedirectToAction("Index");
 		}
@@ -76,7 +76,7 @@ public class CategoryController : Controller
 		{
 			return NotFound();
 		}
-		Category? categoryFromDb = _db.Categories.Find(id);
+		Category? categoryFromDb = _categoryRepo.Get(u => u.Id == id);
 
 		if (categoryFromDb == null)
 		{
@@ -87,13 +87,13 @@ public class CategoryController : Controller
 	[HttpPost, ActionName("Delete")]
 	public IActionResult DeletePOST(int? id)
 	{
-		Category? obj = _db.Categories.Find(id);
+		Category? obj = _categoryRepo.Get(u => u.Id == id);
 		if (obj == null)
 		{
 			return NotFound();
 		}
-		_db.Categories.Remove(obj);
-		_db.SaveChanges();
+		_categoryRepo.Remove(obj);
+		_categoryRepo.Save();
 		TempData["success"] = "Category deleted successfully";
 		return RedirectToAction("Index");
 	}
--- StanimalisThriftShop/Program.cs
+++ StanimalisThriftShop/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using StanimalisThriftShop.DataAccess.Data;
+using StanimalisThriftShop.DataAccess.Repository;
+using StanimalisThriftShop.DataAccess.Repository.IRepository;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +10,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options=> 
 	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 
 var app = builder.Build();
 
