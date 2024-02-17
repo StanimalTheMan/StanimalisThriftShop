a1bee0ca5b30867291ff1a2aeca1016a78ea5f1c
Stan Choi <sjc591@nyu.edu>
2024-02-17 15:15:03 -0500

    Implement Category Repository

---
+++ StanimalisThriftShop.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,24 @@
+﻿using StanimalisThriftShop.DataAccess.Data;
+using StanimalisThriftShop.DataAccess.Repository.IRepository;
+using StanimalisThriftShop.Models;
+
+namespace StanimalisThriftShop.DataAccess.Repository;
+
+public class CategoryRepository : Repository<Category>, ICategoryRepository
+{
+	private ApplicationDbContext _db;
+    public CategoryRepository(ApplicationDbContext db) : base(db)
+    {
+        _db = db;
+    }
+
+    public void Save()
+	{
+		_db.SaveChanges();
+	}
+
+	public void Update(Category obj)
+	{
+		_db.Categories.Update(obj);
+	}
+}
