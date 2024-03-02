a4e57e95a3d757ca1e6ab0aa389486231da33f97
Stan Choi <sjc591@nyu.edu>
2024-03-02 10:34:15 -0500

    Use ViewData as alternative for ViewBag is just a wrapper for it

--- StanimalisThriftShop/Areas/Admin/Controllers/ProductController.cs
+++ StanimalisThriftShop/Areas/Admin/Controllers/ProductController.cs
@@ -28,7 +28,8 @@ public class ProductController : Controller
                 Value = u.Id.ToString()
             });
 
-        ViewBag.CategoryList = CategoryList;
+        //ViewBag.CategoryList = CategoryList;
+        ViewData["CategoryList"] = CategoryList;
 
         return View();
     }
