fc5db7b50c55ae2d9f02a9962291d997c6dbe2ba
Stan Choi <sjc591@nyu.edu>
2024-03-03 09:19:37 -0500

    Add Identity in .NET Core, Register Application User, Create Roles in Database, Assign Roles upon Registration

--- StanimalisThriftShop.DataAccess/Migrations/20240303130749_ExtendIdentityUser.Designer.cs
+++ StanimalisThriftShop.DataAccess/Migrations/20240303130749_ExtendIdentityUser.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using StanimalisThriftShop.DataAccess.Data;
 
@@ -11,9 +12,11 @@ using StanimalisThriftShop.DataAccess.Data;
 namespace StanimalisThriftShop.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240303130749_ExtendIdentityUser")]
+    partial class ExtendIdentityUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
